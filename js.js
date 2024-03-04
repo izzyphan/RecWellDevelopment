925ccbb5b5260e5c7ab9aac3e915af38cf8bb105
Emily <ewermeling@wisc.edu>
2024-03-04 13:42:46 -0600

    added js to log in modal to create new user

--- js.js
+++ js.js
@@ -112,3 +112,22 @@ document.getElementById("logo").addEventListener("click", function (event) {
     })
     .catch((error) => console.error("Error fetching HTML:", error));
 });
+
+// Add event listener to login form submission
+r_e("loginform").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Get form input values
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Log form input values to the console
+  console.log("Username:", username);
+  console.log("Password:", password);
+
+  auth.createUserWithEmailAndPassword(username, password).then((cred) => {
+    console.log(cred);
+  });
+});
+
+console.log("hi");
